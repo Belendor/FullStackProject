f657494110e01accb1f90ade2a6caf0b9263366b
Belendor <dwarf11@gmail.com>
2020-05-27 21:16:22 +0300

    Updates

---
+++ precache-manifest.a207ef17f3e9cd0aaaf76894f97120e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6daa43f779b8a1d376a44cae5b9ff54",
+    "url": "/FullStackProject/index.html"
+  },
+  {
+    "revision": "50abccdf9b7a2a4b11aa",
+    "url": "/FullStackProject/static/css/2.6d92adba.chunk.css"
+  },
+  {
+    "revision": "50abccdf9b7a2a4b11aa",
+    "url": "/FullStackProject/static/js/2.baa54c0b.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/FullStackProject/static/js/2.baa54c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2330a908311e53a0b7",
+    "url": "/FullStackProject/static/js/main.fa4cec15.chunk.js"
+  },
+  {
+    "revision": "bde8dd1ff42372bfb13e",
+    "url": "/FullStackProject/static/js/runtime-main.c033b150.js"
+  }
+]);
